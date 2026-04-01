f94ca388e7b7c1a70601468826931874ef37fe76
Bartosz1Tarka <133283787+Bartosz1Tarka@users.noreply.github.com>
2026-04-01 21:19:23 +0200

    main.py
    Rain allert program

--- main.py
+++ main.py
@@ -6,33 +6,58 @@
 # See the solution video in the 100 Days of Python Course for explainations.
 
 
-from datetime import datetime
-import pandas
-import random
-import smtplib
+import requests
+from twilio.rest import Client
 import os
+from dotenv import load_dotenv
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+load_dotenv()
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+twilio_account_sid = os.getenv("TWILIO_SID")
+twilio_auth_token = os.getenv("TWILIO_TOKEN")
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+#OPENWEATHERMAP
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+#wroclaw
+#lon = 17.038538
+#lat = 51.107883
+lon = 23.045185
+lat = 53.967057
+
+owm_endpoint = "https://api.openweathermap.org/data/2.5/forecast?"
+owm_api_key = os.getenv("OWM_API_KEY")
+
+weather_params = {
+    "lat": lat,
+    "lon": lon,
+    "appid": owm_api_key,
+    "cnt": 4
+}
+
+response = requests.get(owm_endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+
+#print(weather_data["list"][0]["weather"][0]["id"])
+
+will_rain = False
+
+for item in weather_data["list"]:
+    condition_code = item["weather"][0]["id"]
+    if int(condition_code) < 900:
+        will_rain = True
+
+if will_rain:
+    
+    client = Client(twilio_account_sid, twilio_auth_token)
+    message = client.messages.create(
+    from_="whatsapp:+14155238886",
+    body="It's going to rain today. Remember to bring an umbrella",
+    to="whatsapp:+48534409400"
+    )
+    print("Bring an umbrella.")
+    print(message.sid)
+    message = client.messages(message.sid).fetch()
+    print(message)
+    print(message.error_code)
+    print(message.error_message)
